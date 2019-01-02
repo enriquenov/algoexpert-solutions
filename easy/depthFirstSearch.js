cbb29492614f5762c1f29c33d028b6d0ec19411b
enrique novoa <luisenriquenovoa@gmail.com>
2019-01-02 10:56:17 -0800

    Depth First Search solved.

---
+++ easy/depthFirstSearch.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    array.push(this.name);
+
+    this.children.forEach(child => {
+      child.depthFirstSearch(array);
+    });
+
+    return array;
+  }
+}
