b584d7b84e43a1dbbd412bc92957764d455ae1ac
enrique novoa <luisenriquenovoa@gmail.com>
2019-02-28 22:53:23 -0800

    isPalindrome optimally solved.

--- easy/isPalindrome.js
+++ easy/isPalindrome.js
@@ -1,10 +1,24 @@
-function isPalindrome(string) {
-  if (string.length === 1 || !string.length) return true;
+// function isPalindrome(s) {
+//   if (s.length === 1 || !s.length) return true;
 
-  return (
-    string[0] === string[string.length - 1] &&
-    isPalindrome(string.slice(1, string.length - 1))
-  );
+//   return s[0] === s[s.length - 1] && isPalindrome(s.slice(1, s.length - 1));
+// }
+
+function isPalindrome(s) {
+  let first = 0;
+  let last = s.length - 1;
+  let isPal = true;
+
+  while (isPal && first < last && s.length > 1) {
+    if (s[first] !== s[last]) {
+      isPal = false;
+    }
+
+    first++;
+    last--;
+  }
+
+  return isPal;
 }
 
 console.log(isPalindrome('abcdcba'));
