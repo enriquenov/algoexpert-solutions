b5d5ddc63afeffab4e95267f5160459e669324b4
enrique novoa <luisenriquenovoa@gmail.com>
2019-03-05 15:09:54 -0800

    getPermutations helper function refactored.

--- medium/getPermutations.js
+++ medium/getPermutations.js
@@ -1,19 +1,21 @@
+const permutationsHelper = (array, currentPermutation, permutations) => {
+  if (!array.length && currentPermutation.length) {
+    permutations.push(currentPermutation);
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      const newArray = array.slice(0, i).concat(array.slice(i + 1));
+      const newPermutation = currentPermutation.concat(array[i]);
+      permutationsHelper(newArray, newPermutation, permutations);
+    }
+  }
+};
+
 function getPermutations(arr) {
   const permutations = [];
 
   permutationsHelper(arr, [], permutations);
 
-  const permutationsHelper = (array, currentPermutation, permutations) => {
-    if (!array.length && currentPermutation.length) {
-      permutations.push(currentPermutation);
-    } else {
-      for (let i = 0; i < array.length; i++) {
-        const newArray = array.slice(0, i).concat(array.slice(i + 1));
-        const newPermutation = currentPermutation.concat(array[i]);
-        permutationsHelper(newArray, newPermutation, permutations);
-      }
-    }
-  };
+  return permutations;
 }
 
 console.log(getPermutations([1, 2, 3]));
