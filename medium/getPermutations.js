d4e510304db6aa1a944cd48efcc39d605eecd06a
enrique novoa <luisenriquenovoa@gmail.com>
2019-03-04 23:48:34 -0800

    getPermutations solved.

--- medium/getPermutations.js
+++ medium/getPermutations.js
@@ -1,5 +1,19 @@
 function getPermutations(arr) {
-  /// Write
+  const permutations = [];
+
+  permutationsHelper(arr, [], permutations);
+
+  const permutationsHelper = (array, currentPermutation, permutations) => {
+    if (!array.length && currentPermutation.length) {
+      permutations.push(currentPermutation);
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        const newArray = array.slice(0, i).concat(array.slice(i + 1));
+        const newPermutation = currentPermutation.concat(array[i]);
+        permutationsHelper(newArray, newPermutation, permutations);
+      }
+    }
+  };
 }
 
 console.log(getPermutations([1, 2, 3]));
