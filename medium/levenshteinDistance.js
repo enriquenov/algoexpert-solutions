da44e1ebbc1417adba6d6b8e4f2b6e30d8dca324
enrique novoa <luisenriquenovoa@gmail.com>
2019-03-18 17:46:12 -0700

    levenshteinDistance solved.

---
+++ medium/levenshteinDistance.js
@@ -0,0 +1,32 @@
+function levenshteinDistance(str1, str2) {
+  let twodarray = [];
+
+  for (let i = 0; i < str2.length + 1; i++) {
+    let row = [];
+    for (let j = 0; j < str1.length + 1; j++) {
+      row.push(j);
+    }
+    row[0] = i;
+    twodarray.push(row);
+  }
+
+  for (let r = 1; r < str2.length + 1; r++) {
+    for (let c = 1; c < str1.length + 1; c++) {
+      if (str2[r - 1] === str1[c - 1]) {
+        twodarray[r][c] = twodarray[r - 1][c - 1];
+      } else {
+        twodarray[r][c] =
+          1 +
+          Math.min(
+            twodarray[r][c - 1],
+            twodarray[r - 1][c],
+            twodarray[r - 1][c - 1]
+          );
+      }
+    }
+  }
+
+  return twodarray[str2.length][str1.length];
+}
+
+console.log(levenshteinDistance('abc', 'yabd'));
