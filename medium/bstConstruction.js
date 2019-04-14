8877a9c6bef8f8c8f815d2207fd9bfa690aff4c8
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-14 13:15:24 -0700

    bstConsutruction solved.

---
+++ medium/bstConstruction.js
@@ -0,0 +1,97 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+    }
+    return this;
+  }
+
+  contains(value) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(value, parentNode = null) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left !== null && currentNode.right !== null) {
+          currentNode.value = currentNode.right.getMinValue();
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (parentNode === null) {
+          if (currentNode.left !== null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          } else {
+            currentNode.value = null;
+          }
+        } else if (parentNode.left === currentNode) {
+          parentNode.left =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        }
+        break;
+      }
+    }
+    return this;
+  }
+
+  getMinValue() {
+    let currentNode = this;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+}
+
+let bst = new BST(10)
+  .insert(5)
+  .insert(15)
+  .insert(5)
+  .insert(2)
+  .insert(14)
+  .insert(22);
+
+console.log('BinarySearchTree: ', bst);
